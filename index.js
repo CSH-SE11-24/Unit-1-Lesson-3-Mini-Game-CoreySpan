8dc336f1716b70997db0050059cbd533b43f7507
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:24:45 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// Get started!
+const name = prompt("What's your name?");
+
+const age = 16;
+
+console.log(`You are, ${age},years old` );
+
+console.log(`Welcome to my game, ${name}` );
+
+let choice = prompt(`What sport do you want to play?, (football, basketball, soccer)`);
+
+console.log(`Okay, ${name}, your sport is now ${choice}`);
+
+let role = prompt("What is your role?")
+
+role = parseInt(role)
